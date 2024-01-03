f46da40b34142761f267b93dd59b3805326f216c
Mihar025 <115378551+Mihar025@users.noreply.github.com>
2024-01-02 19:50:17 -0500

    Add files via upload

---
+++ spring_course_rest/src/main/java/com/zaurtregulov/spring/rest/exception_handling/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.zaurtregulov.spring.rest.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeGlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorectData>handleexception(
+            NoSuchEmployeeException exception
+    ){
+        EmployeeIncorectData data = new EmployeeIncorectData();
+        data.setData(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorectData>handleexception(
+            Exception exception
+    ){
+        EmployeeIncorectData data = new EmployeeIncorectData();
+        data.setData(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+
+
+
+
+}
---
+++ spring_course_rest/src/main/java/com/zaurtregulov/spring/rest/exception_handling/EmployeeIncorectData.java
@@ -0,0 +1,17 @@
+package com.zaurtregulov.spring.rest.exception_handling;
+
+public class EmployeeIncorectData  {
+    
+    private String data;
+
+    public EmployeeIncorectData() {
+    }
+
+    public String getData() { 
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
